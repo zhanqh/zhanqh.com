a3410948e1de740a5fa423f2da3ae3908be8e02f
zhanqh <scnu2017@gmial.com>
2016-09-05 20:22:20 +0800

    v0.0.1

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ var bus = require('./routes/bus')
 app.use('/', home)  // 主页
 app.use('/blog', blog)	// 博客
 app.use('/bus', bus) // 公交查询
-app.listen(3030)
+app.listen(80)
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-console.log("服务器启动成功，地址：127.0.0.1:3030")
+console.log("服务器启动成功，地址：127.0.0.1:80")
